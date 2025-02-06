063254364340d6cafc41b5114c6ec6f45765214a
leo-090804 <LeoLincoln9804@gmail.com>
2025-02-06 23:30:04 +0700

    eda stuffs

--- data_pipeline/src/process_satellite_image.py
+++ data_pipeline/src/process_satellite_image.py
@@ -2,6 +2,7 @@ import yaml
 import rasterio
 import pystac_client
 import planetary_computer 
+import rioxarray
 from odc.stac import stac_load
 
 # lon low, lat low, lon upp, lat upp
@@ -72,6 +73,8 @@ def process_landsat(config, data):
 def save_raster(data, filename):
     height = data.dims["latitude"]
     width = data.dims["longitude"]
+        
+    # print(height, width)
 
     gt = rasterio.transform.from_bounds(COORDS[0], COORDS[1], COORDS[2], COORDS[3], width, height)
     data.rio.write_crs("epsg:4326", inplace=True)
@@ -97,4 +100,3 @@ if __name__ == "__main__":
 
     data_sentinel = load_image(config, "sentinel_2")
     save_raster(data_sentinel, "data_pipeline/data/tiff/sentinel_2.tiff")
-
